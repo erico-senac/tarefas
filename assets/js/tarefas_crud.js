0c755b409c8d9e4ebedda7952a903faa7ae06738
erico-senac <professorerico@pi.senac.br>
2024-05-28 21:54:47 -0300

    usando localstorage

--- assets/js/tarefas_crud.js
+++ assets/js/tarefas_crud.js
@@ -1,17 +1,53 @@
-//
-// tarefa = {
-//     nome : "Nome",
-//     detalhes : "O que é",
-//     classificacao : "qual é",
-//     prazo: "data",
-//     status: "qual o status"
-// };
-
-const button_save = document.querySelector('#btn_save');
+/*botões*/
+const btnAddTask = document.querySelector('#btn_add');
+const btnSaveTask = document.querySelector('#btn_save');
+const btnCancelTask = document.querySelector('#btn_cancel');
+/*form*/
+const formulario = document.querySelector('.app__form_add_tasks');
+const div_formulario = document.querySelector('.app__form');
+/*inputs da task*/
 const name_task = document.querySelector('#name_task');
 const task = document.querySelector('#task');
+const class_task = document.querySelector('#class_task');
+const date_task = document.querySelector('#date_task');
+const status_task = document.querySelector('#status_task');
 
-button_save.addEventListener('click', (e) => {
+/*Create Task*/
+const inserirDados = (tarefa) => {
+    localStorage.setItem(tarefa.nome, JSON.stringify(tarefa.detalhes));
+}
+/*exibir formulário*/
+const btnToggle = () => {
+    formulario.classList.toggle('hidden');
+    div_formulario.classList.toggle('app__form_grid');
+    btnAddTask.classList.toggle("hidden");
+}
+btnAddTask.addEventListener('click', (e) => {
+    btnToggle();
+    /*formulario.classList.remove('hidden');
+    div_formulario.classList.remove('app__form_grid');
+    e.target.classList.add("hidden");*/
+});
+btnCancelTask.addEventListener('click', (e) => {
+    /*formulario.classList.add('hidden');
+    div_formulario.classList.add('app__form_grid');
+    btnAddTask.classList.remove("hidden");*/
+    btnToggle();
+});
+btnSaveTask.addEventListener('click', (e) => {
+    let tarefa = {
+        'nome': name_task.value,
+        'detalhes' : {
+            'descricao': task.value,
+            'classificacao': class_task.value,
+            'prazo': date_task.value,
+            'status': status_task.value
+        }
+    }
+    inserirDados(tarefa);
+});
+formulario.addEventListener('submit', (e) => {
     e.preventDefault();
-    alert(name_task.value);
+    e.target.reset();
+    btnToggle();
 })
